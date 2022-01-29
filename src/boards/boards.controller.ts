ab53a1b99f4788f1e83550d2a0e03b89cb714894
ymink716 <ymink716@gmail.com>
2022-01-29 17:45:24 +0900

    feat: board 조회, 수정, 삭제 api 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,19 +1,37 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Board } from './boards.model';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
-  constructor(private BoardsService: BoardsService) {}
+  constructor(private boardsService: BoardsService) {}
 
   @Get('/')
   getAllBoard(): Board[] {
-    return this.BoardsService.getAllBoards();
+    return this.boardsService.getAllBoards();
   }
 
   @Post('/')
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    return this.BoardsService.createBoard(createBoardDto);
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ): Board {
+    return this.boardsService.updateBoardStatus(id, status);
   }
 }
