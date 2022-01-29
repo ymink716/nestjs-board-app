2ebbc2afac5230e3314e8bbeb142e00aa4e72ff2
ymink716 <ymink716@gmail.com>
2022-01-29 19:20:04 +0900

    feat: typeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.test.env',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule,
+  ],
 })
 export class AppModule {}
